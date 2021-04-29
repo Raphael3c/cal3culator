625ad73125c13aeed5bba715588a801957cc5d49
Raphael3c <phael.ufc@gmail.com>
2021-04-29 14:57:34 -0300

    Utilizing JsonServer, Axios and map to build the buttons

--- src/components/Keyboard.jsx
+++ src/components/Keyboard.jsx
@@ -1,21 +1,39 @@
-
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 import '../styles/keyboard.css'
 
 export function Keyboard(){
 
+  const [buttons, setButtons] = useState([])
+
   function handle(e){
-  console.log(e)
+    console.log(e)
   }
+
+  useEffect(() => { 
+    axios.get('http://localhost:3333/buttons')
+      .then(res => {
+        const data = res.data;
+
+        setButtons(data)
+      })
+  }, [])
 
   return (
     <div className="container">
-      <button 
-        type="button" 
-        value="asdasd" 
-        onClick={e => handle(e.target.value)}
-      >
-          teste
-      </button>
+      {buttons.map((button) => {
+        return (
+            <button 
+            type="button" 
+            value={button.id} 
+            onClick={e => handle(e.target.value)}
+            key={button.id}
+            className={button.className}
+          >
+              {button.id}
+          </button>
+        )
+      })}
     </div>
   )
 }
