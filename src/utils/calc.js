42d62e788e1d28e228705670db9949b16056bad5
Raphael3c <phael.ufc@gmail.com>
2021-05-02 14:10:54 -0300

    function sum

--- src/utils/calc.js
+++ src/utils/calc.js
@@ -1,8 +1,19 @@
 export const calculadora = {
   trata: (expression) => {
-    
-  },
+    const multi = /[x]/i;
+    const soma = /[+]/i;
+    //const sub = /[-]/i;
+    //const div = /[x]/i;
 
+    if(multi.test(expression)){
+      return calculadora.multiplicacao(expression)
+    }
+
+    if(soma.test(expression)){
+     return calculadora.soma(expression)
+    }
+
+  },
 
   multiplicacao: (expression) => {
     const [numero1, numero2] = expression.split("x");
@@ -11,12 +22,13 @@ export const calculadora = {
 
     return result;
   },
+  
   soma: (expression) => {
-    const [numero1, numero2] = expression.split("x");
+    const [numero1, numero2] = expression.split("+");
     
-    const result = numero1 + numero2;
+    const result = Number(numero1) + Number(numero2);
 
-    return result;
+    return String(result);
   }
 
 
